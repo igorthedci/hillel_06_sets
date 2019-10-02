9045ece1d617fc91e365cead19de34b17d1c7004
igorthedci <foumart@ukr.net>
2019-10-03 00:25:44 +0300

    Hillel AQA. Home-work #06. Initial commit

---
+++ src/main/java/task_01.java
@@ -0,0 +1,16 @@
+import java.util.ArrayList;
+
+public class task_01 {
+    /**
+     * 1. Create ArrayList and print it.
+     */
+    public static void main(String[] args) {
+        ArrayList aBrands = new ArrayList();
+
+        aBrands.add("Coca-Cola");
+        aBrands.add("Volvo");
+        aBrands.add("CIA");
+
+        System.out.println(aBrands.toString());
+    }
+}

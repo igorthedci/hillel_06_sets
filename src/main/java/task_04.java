c81980162c0a5540331475223f717a5c7a16d337
igorthedci <foumart@ukr.net>
2019-10-03 00:29:03 +0300

    task 04. random array of 15 items

---
+++ src/main/java/task_04.java
@@ -0,0 +1,8 @@
+public class task_04 {
+    /**
+     * Create an array of 15 random integers from the segment [0;   9].
+     * Display the array on the screen.
+     * Count how many in the array of even elements
+     * and display this number on the screen on a separate line.
+     */
+}
